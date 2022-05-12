838db9061b2b643b6ec1a897c8747fa325ce2a12
Bob D <txcavscout@live.com>
2022-05-11 23:32:52 -0700

    Add project files.

---
+++ HomeWork6/Program.cs
@@ -0,0 +1,18 @@
+﻿List<string> classRoster = new List<string>();
+string inserts;
+
+do
+{
+    Console.Write("Enter student name. (type end to quit): ");
+    inserts = Console.ReadLine();
+
+    if (inserts != "end")
+    {
+        classRoster.Add(inserts);
+    }
+    else if (inserts == "end")
+    {
+        Console.WriteLine(classRoster.Count + " students in the class.");
+    }
+
+} while (inserts != "end");
